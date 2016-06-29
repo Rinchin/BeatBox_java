bc152949c2b515e3942e1dedd6ac764eaf2ecac7
Rinchin <gomboev.rinchin@gmail.com>
2016-06-30 02:16:45 +0300

    Add code comments

--- BeatBox.java
+++ BeatBox.java
@@ -10,16 +10,23 @@ import java.util.ArrayList;
  */
 public class BeatBox {
     JPanel mainPanel;
+
+    //Here I store flags
     ArrayList<JCheckBox> checkboxList;
     Sequencer sequencer;
     Sequence sequence;
     Track track;
     JFrame theFrame;
 
+    //Intrument names - for labels in user interface
     String[] instrumentNames = {"Bass Drum","Closed Hi-Hat","Open Hi-Hat","Acoustic Snare","Crash Cymbal",
         "Hand Clap","High Tom","Hi Bongo","Maracas","Whistle","Low Conga","Cowbell","Vibraslap","Low-mid Tom",
         "High Agogo", "Open Hi Conga"};
 
+    //Drum keys = like piano
+    //35 = Bass Drum ...
+    //...
+    //63 = Open Hi Conga
     int[] instruments = {35,42,46,38,49,39,50,60,70,72,64,56,58,47,67,63};
 
     public static void main(String[] args){
@@ -32,6 +39,8 @@ public class BeatBox {
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         BorderLayout layout = new BorderLayout();
         JPanel background = new JPanel(layout);
+
+        //Padding 10
         background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 
         checkboxList = new ArrayList<JCheckBox>();
@@ -64,17 +73,18 @@ public class BeatBox {
         theFrame.getContentPane().add(background);
 
         GridLayout grid = new GridLayout(16,16);
-       // grid.setVgap(1);
         grid.setHgap(2);
         mainPanel = new JPanel(grid);
         background.add(BorderLayout.CENTER, mainPanel);
 
+
+        //Create empty flags with value false and then add them to ArrayList and to Panel
         for (int i=0;i<256;i++){
             JCheckBox c = new JCheckBox();
             if (i==16) c.setSelected(true);
             else c.setSelected(false);
-            checkboxList.add(c);
-            mainPanel.add(c);
+            checkboxList.add(c); //add checkbox to ArrayList
+            mainPanel.add(c); // Add checkbox to Panel
         }
 
         setUpMidi();
@@ -84,6 +94,7 @@ public class BeatBox {
         theFrame.setVisible(true);
     }
 
+    //Create MIDI code for synthesizer, sequencer and track
     private void setUpMidi() {
         try{
             sequencer = MidiSystem.getSequencer();
@@ -96,33 +107,38 @@ public class BeatBox {
     }
 
     public void buildTrackAndStart(){
+        //Create array with 16 elements = intruments
         int[] trackList = null;
 
+        //delete old track
         sequence.deleteTrack(track);
+        //add new track
         track = sequence.createTrack();
 
+        //do it for all 16 instruments = Bass, Congo ...
         for (int i = 0;i<16;i++){
             trackList = new int[16];
 
+            //Key = instrument key
             int key = instruments[i];
 
             for (int j=0;j<16;j++){
                 JCheckBox jc = checkboxList.get(j+(16*i));
-                if (jc.isSelected())
+                if (jc.isSelected()) //is tick checked then press key
                     trackList[j] = key;
                 else
-                    trackList[j] = 0;
+                    trackList[j] = 0; //nothing to do
             }
 
-            makeTracks(trackList);
+            makeTracks(trackList);//for this instrument and for all 16 ticks create events and add them to track
             track.add(makeEvent(176,1,127,0,16));
         }
 
-        track.add(makeEvent(192,9,1,0,15));
+        track.add(makeEvent(192,9,1,0,15)); //ckecking that event exists from 0 to 15 ticks
         try{
             sequencer.setSequence(sequence);
-            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
-            sequencer.start();
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY); //Infinite loop for melody,
+            sequencer.start(); // melody start
             sequencer.setTempoInBPM(120);
         }
         catch (Exception e){
@@ -130,13 +146,16 @@ public class BeatBox {
         }
     }
 
+
+    //method creates events for 1 instrument for all 16 ticks
+    //if Get Array int[] for Instrument[Bass Drum] every element will be 0 or this instrument key
     private void makeTracks(int[] list) {
         for (int i = 0 ; i<16;i++){
             int key = list[i];
 
             if (key!=0){
-                track.add(makeEvent(144,9,key,100,i));
-                track.add(makeEvent(144,9,key,100,i+1));
+                track.add(makeEvent(144,9,key,100,i)); // start play note
+                track.add(makeEvent(144,9,key,100,i+1)); // stop play note
             }
         }
     }
